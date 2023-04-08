f9e0abfe209134b0c2a664cea8646dd13cda0368
benwaples <benwaples@gmail.com>
2023-04-07 19:35:19 -0700

    add middleware

--- src/pages/api/estate-sale/all-upcoming-sales.ts
+++ src/pages/api/estate-sale/all-upcoming-sales.ts
@@ -2,6 +2,7 @@ import { Dictionary, NextApiReq, NextApiRes } from "@/types";
 import { parseResponseBodyIntoDom, parseSaleAddress, parseSaleDateString, removeTabsAndNewLines } from "./utils";
 import { NextApiRequest, NextApiResponse } from "next";
 import User from "../model/User";
+import { checkAuthMiddleware } from "../auth/utils";
 
 export async function allUpcomingSalesIds() {
 	const response = await fetch('https://www.estatesale-finder.com/all_sales_list.php?saletypeshow=1&regionsshow=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,31,32,33,34,23,24,25,26,27,28,29,35,36,37&proonly=false&todayonly=false');
@@ -64,10 +65,12 @@ export async function getSaleInfo(id: number) {
 	return data
 }
 
-export default async function allUpcomingSales(
+async function allUpcomingSales(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
+	if (req.method !== 'GET') return res.status(404);
+
 	try {
 		const saleIds = await allUpcomingSalesIds();
 
@@ -79,3 +82,5 @@ export default async function allUpcomingSales(
 		res.status(500).send(e.message)
 	}
 }
+
+export default checkAuthMiddleware(allUpcomingSales)
