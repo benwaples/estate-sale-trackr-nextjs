c5bb096e2e315ad2c455a29edac3dd45133db097
benwaples <benwaples@gmail.com>
2023-04-10 14:01:12 -0700

    okay auth but doesnt feel right

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Router from 'next/router'
 import styles from '../styles/Home.module.scss'
+import { useEffect, useState } from 'react';
+import useAuth from '@/utils/use-auth';
 
 function Home() {
+  const { token } = useAuth();
+
   return <h1 className={styles.test}>HOMEEE</h1>
 }
 
