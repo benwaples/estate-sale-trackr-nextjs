dacc13bd5c91415486f487de76a9f97c7028ae65
benwaples <benwaples@gmail.com>
2023-04-11 11:04:30 -0700

    adds favicon

---
+++ src/components/estate-sale/estate-sale-list.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props { }
+
+function EstateSaleList() {
+
+	return (
+		<div className="estate-sale-list">
+
+		</div>
+	)
+}
+
+export default EstateSaleList
