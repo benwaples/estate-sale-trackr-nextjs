8061d6a4ebadb3a084223ff19372d9c61c0aeebd
benwaples <benwaples@gmail.com>
2023-04-27 10:13:29 -0700

    remove context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from "next-auth/react";
-import EstateSaleContext, { defaultEstateSaleContext } from '../context/estate-sales';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <EstateSaleContext.Provider value={defaultEstateSaleContext}>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </EstateSaleContext.Provider>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 }
