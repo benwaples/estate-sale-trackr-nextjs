cdd957dec4fecf4241c0abc7fe2f902032441c94
benwaples <benwaples@gmail.com>
2023-05-01 14:00:09 -0700

    better info box

--- src/pages/map/infobox.tsx
+++ src/pages/map/infobox.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from '../../styles/map.module.scss';
 import { CoordinateSaleData } from '@/types';
 import { InfoBoxF } from '@react-google-maps/api';
+import Slider, { Settings } from 'react-slick';
 
 interface Props {
 	saleList: CoordinateSaleData[];
@@ -12,9 +13,15 @@ interface Props {
 function InfoBox(props: Props) {
 	const { saleList, onSaleClick, onClose } = props;
 
+	const sliderRef = useRef<Slider | null>(null);
+
 	const handleSaleClick: (id: number) => React.MouseEventHandler<HTMLDivElement> = (id) => async (e) => {
 		e.stopPropagation();
 		await onSaleClick(id);
+	};
+
+	const sliderConfig: Settings = {
+		arrows: false
 	};
 
 	return (
@@ -22,19 +29,27 @@ function InfoBox(props: Props) {
 			position={new google.maps.LatLng(saleList[0].coordinates)}
 			options={{ closeBoxURL: '', disableAutoPan: true, boxClass: styles.infoBoxS, visible: !!saleList, alignBottom: true }}
 		>
-			<div className={styles.infoBoxContainer} >
-				<p>{saleList.length} sales in this zipcode without adddress posted</p>
+			<div className={styles.infoBoxContainer} onTouchMove={e => e.stopPropagation()} >
+				<p><strong>{saleList.length} sales</strong> in this zip code without addresses posted.</p>
 				<br />
 				<div className={styles.matchingSaleContainer}>
-					{saleList.map(sale => {
-						return (
-							<div className={styles.matchingSale} key={sale.id} onClick={handleSaleClick(sale.id)}>
-								{sale.address}
-							</div>
-						);
-					})}
+					<Slider {...sliderConfig} ref={ref => sliderRef.current = ref}>
+						{saleList.map((sale, i) => {
+							return (
+								<div className={styles.matchingSale} key={sale.id} onClick={handleSaleClick(sale.id)}>
+									<strong>{i + 1}.</strong>
+									<br />
+									{sale.address} <button className={styles.saleDetailsBtn}>See Details</button>
+								</div>
+							);
+						})}
+					</Slider>
+					<div className={styles.sliderController}>
+						<button onClick={() => sliderRef.current?.slickPrev()}>&lt;</button>
+						<button onClick={() => sliderRef.current?.slickNext()}>&gt;</button>
+					</div>
 				</div>
-				<button onClick={onClose}>close</button>
+				<button className={styles.closeBtn} onClick={onClose}>close</button>
 			</div>
 		</InfoBoxF>
 	);
