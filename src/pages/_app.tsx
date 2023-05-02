0d60c46ad838964ad98babc4b73f4abc15d68e51
benwaples <benwaples@gmail.com>
2023-05-01 17:13:20 -0700

    seems to have worked for the mobile height

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from 'react';
 import '@/styles/globals.css';
 import useFavicon from '@/hooks/useFavicon';
 
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const favicon = useFavicon();
+
+  useEffect(() => {
+    const documentHeight = () => {
+      const doc = document.documentElement;
+      doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
+    };
+    window.addEventListener('resize', documentHeight);
+    documentHeight();
+    return () => {
+      window.removeEventListener('resize', documentHeight);
+    };
+  }, []);
 
   return (
     <>
