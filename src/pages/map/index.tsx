15614475d763bfe796d049797509151d149a1c3a
benwaples <benwaples@gmail.com>
2023-05-04 15:49:26 -0700

    set map card view to minimized on load if there is a sale id

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -112,6 +112,7 @@ function Map(props: Props) {
 	const onMapLoad = useCallback(async (map: google.maps.Map) => {
 		mapRef.current = map;
 		if (firstSaleInfo) {
+			setSaleView(MobileMapSaleViewType.minimized);
 			await getSaleDetails(firstSaleInfo.id, firstSaleInfo.coordinates);
 		}
 	}, [firstSaleInfo, getSaleDetails]);
