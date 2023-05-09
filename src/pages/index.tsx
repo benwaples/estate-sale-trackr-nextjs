b065fcc27602c23a1d2d3e2fc5bd9855b942d9ee
benwaples <benwaples@gmail.com>
2023-05-09 15:28:30 -0700

    remove unused values in redirect page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,10 @@
-import Head from 'next/head';
-import Nav from '@/components/nav/nav';
 import { useEffect, useState } from 'react';
 import Router, { useRouter } from 'next/router';
-import EstateSaleList from '@/pages/list';
-import { allUpcomingSaleIds } from './api/estate-sale/all-upcoming-sales';
-import { BaseSaleData, CoordinateSaleData } from '@/types';
-import Map from '@/pages/map';
 
 
-// export const getServerSideProps = async () => {
-//   const saleInfo = await allUpcomingSaleIds(true);
-//   return { props: { saleInfo } }; // will be passed to the page component as props
-// };
+function Home() {
 
-interface Props {
-  saleInfo?: CoordinateSaleData[];
-}
-
-function Home(props: Props) {
-  const { saleInfo } = props;
-
-  const { query, pathname } = useRouter();
+  const { pathname } = useRouter();
 
   useEffect(() => {
     if (!pathname.includes('map') && !pathname.includes('list')) {
@@ -30,16 +14,6 @@ function Home(props: Props) {
 
   return (
     <>
-      <div style={{ minHeight: '100vh', display: 'flex' }}>
-
-        {/* <Nav /> */}
-        {/* TODO: add support for a calendar view */}
-        {/* {displayType === 'list' ? (
-          <EstateSaleList saleInfo={saleInfo ?? []} />
-        ) : (
-          <Map saleInfo={saleInfo ?? []} />
-        )} */}
-      </div>
     </>
   );
 }
